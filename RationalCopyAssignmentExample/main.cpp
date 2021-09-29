ef441da19615b1af5334d760babcf61d2d9fb021
Dave Reed <dave256@mac.com>
2021-09-29 09:40:27 -0400

    Initial Commit

---
+++ RationalCopyAssignmentExample/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RationalCopyAssignmentExample
+//
+//  Created by David M. Reed on 9/29/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
